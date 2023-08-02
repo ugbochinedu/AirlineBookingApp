1f28bb71a85514954895e00ab2a21af84cab1600
OlaGreat <oladipupolamilekan2@gmail.com>
2023-08-02 18:22:08 +0100

    first commit

---
+++ src/main/java/com/example/airlinebookingapp/AirlineBookingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.airlinebookingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineBookingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirlineBookingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/airlinebookingapp/AirlineBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airlinebookingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineBookingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
