5b6d7ee5b18ee8820853160666ecee7090e6f2e9
OlaGreat <oladipupolamilekan2@gmail.com>
2023-08-06 14:06:44 +0100

    update

--- src/main/java/AirlineApp/repositories/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package AirlineApp.repositories;
-
-import AirlineApp.models.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-}
--- src/main/java/AirlineApp/repositories/PassengerRepository.java
+++
@@ -1,7 +0,0 @@
-package AirlineApp.repositories;
-
-import AirlineApp.models.Passenger;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PassengerRepository extends JpaRepository<Passenger, Long> {
-}
